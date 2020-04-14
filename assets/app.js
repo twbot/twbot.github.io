f250085af8cd78ea2b1b20984546311439f19040
Tristan Brodeur <brodeurtristan@gmail.com>
2020-04-13 23:41:26 -0700

    Updating profile page

--- assets/app.js
+++ assets/app.js
@@ -116,3 +116,23 @@ function scrollFunction() {
 		document.getElementById("navbar").style.backgroundColor = "rgba(245,245,245,1)";
 	}
 }
+
+function showPage(id, text) {
+    var i, tabcontent, links;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
