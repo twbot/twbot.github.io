6a13b28612683961ab665bec8eba4de0f23d3bde
Tristan Brodeur <brodeurtristan@gmail.com>
2020-07-11 14:42:28 -0700

    Adding svg animations; Updating mobile layout

--- assets/app.js
+++ assets/app.js
@@ -108,12 +108,55 @@ var pdfState = {
     zoom: 1
 }
 
+var showSubMenu = false;
+
+// document.onreadystatechange = function(e)
+// {
+//     if (document.readyState === 'complete')
+//     {
+//         determineMenu();
+//     }
+// };
+
 window.onload = function()  {
-    document.getElementById("defaultOpen").click();
-    particlesJS("particles-js", particlesJSON);
+    if(window.innerWidth < 950) {
+        document.getElementById("defaultPhoneOpen").click();
+    }
+    else {
+        document.getElementById("defaultOpen").click();
+    }
+    
+    sphereAnimation();
+    // particlesJS("particles-js", particlesJSON);
 };
 
 window.onscroll = function() {scrollFunction()};
+
+window.onresize = function() {
+    determineMenu();
+}
+
+function determineMenu() {
+
+    if(window.innerWidth < 950) {
+        document.getElementById("navbar-right").classList.add('noShow');
+        document.getElementById("navbar-right-small").classList.remove('noShow');
+    }
+    else {
+        document.getElementById("navbar-right").classList.remove('noShow');
+        document.getElementById("navbar-right-small").classList.add('noShow');
+    }
+}
+
+function showMenu() {
+    this.showSubMenu = !this.showSubMenu;
+    var x = document.getElementById("navbar-right-small-menu");
+    if (showSubMenu) {
+        x.style.display = "block"
+    } else {
+         x.style.display = "none";
+    }
+}
 
 function scrollFunction() {
 	if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
@@ -176,4 +219,92 @@ function download(file) {
     link.href = './assets/pdfs/'+file.toString();
     link.download = file;
     link.dispatchEvent(new MouseEvent('click')); 
+}
+
+function fitElementToParent(el, padding) {
+  var timeout = null;
+  function resize() {
+    if (timeout) clearTimeout(timeout);
+    anime.set(el, {scale: 1});
+    var pad = padding || 0;
+    var parentEl = el.parentNode;
+    var elOffsetWidth = el.offsetWidth - pad;
+    var parentOffsetWidth = parentEl.offsetWidth;
+    var ratio = parentOffsetWidth / elOffsetWidth;
+    timeout = setTimeout(anime.set(el, {scale: ratio}), 10);
+  }
+  resize();
+  window.addEventListener('resize', resize);
+}
+
+function sphereAnimation() {
+
+  var sphereEl = document.querySelector('.sphere-animation');
+  var sphereElSVG = document.querySelector('.sphere');
+  console.log(sphereElSVG)
+  var spherePathEls = sphereElSVG.querySelectorAll('path');
+  var pathLength = spherePathEls.length;
+  var hasStarted = false;
+  var aimations = [];
+
+  fitElementToParent(sphereEl);
+
+  var breathAnimation = anime({
+    begin: function() {
+      for (var i = 0; i < pathLength; i++) {
+        aimations.push(anime({
+          targets: spherePathEls[i],
+          stroke: {value: ['rgba(0,72,38,1)', 'rgba(255,255,255,1)'], duration: 500},
+          translateX: [2, -4],
+          translateY: [2, -4],
+          easing: 'easeOutQuad',
+          autoplay: false
+        }));
+      }
+    },
+    update: function(ins) {
+      aimations.forEach(function(animation, i) {
+        var percent = (1 - Math.sin((i * .35) + (.0022 * ins.currentTime))) / 2;
+        animation.seek(animation.duration * percent);
+      });
+    },
+    duration: Infinity,
+    autoplay: false
+  });
+
+  var introAnimation = anime.timeline({
+    autoplay: false
+  })
+  .add({
+    targets: spherePathEls,
+    strokeDashoffset: {
+      value: [anime.setDashoffset, 0],
+      duration: 3900,
+      easing: 'easeInOutCirc',
+      delay: anime.stagger(190, {direction: 'reverse'})
+    },
+    duration: 2000,
+    delay: anime.stagger(60, {direction: 'reverse'}),
+    easing: 'linear'
+  }, 0);
+
+  var shadowAnimation = anime({
+      targets: '#sphereGradient',
+      x1: '25%',
+      x2: '25%',
+      y1: '0%',
+      y2: '75%',
+      duration: 30000,
+      easing: 'easeOutQuint',
+      autoplay: false
+    }, 0);
+
+  function init() {
+    introAnimation.play();
+    breathAnimation.play();
+    shadowAnimation.play();
+  }
+  
+  init();
+
 }
